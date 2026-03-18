59a9e1bec5f0efd6892906a3d02ed79bb234fb95
priyanshi275yo <priyamudgal275@gmail.com>
2026-03-18 13:21:45 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,7 @@
+document.getElementById("workBtn").onclick = function () {
+    document.getElementById("projects").scrollIntoView({ behavior: "smooth" });
+};
+
+document.getElementById("contactBtn").onclick = function () {
+    document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+};
